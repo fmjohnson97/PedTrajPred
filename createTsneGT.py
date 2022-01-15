a75eda452d0d83df275da1ac47bbf44c080946fd
Faith Johnson <fmjohnson97@aol.com>
2022-01-14 22:40:05 -0500

    tsne ground truth

---
+++ createTsneGT.py
@@ -0,0 +1,206 @@
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+import argparse
+from torch.utils.data import DataLoader
+from sameSizeData import SameSizeData
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from collections import defaultdict
+
+def onclick(event):
+    global ix, iy
+    ix,iy = event.xdata, event.ydata
+    global coords
+    coords=[(ix,iy)]
+    if len(coords)>0:
+        # print(coords)
+        fig.canvas.mpl_disconnect(cid)
+        plt.close(1)
+
+#finds the nearest value in the array of points to the location of the mouse click
+def find_nearest(array,value):
+    temp=np.abs(array - value)
+    small=np.abs(array - value)
+    small.sort()
+    small=small[:10]
+    diff=1e6
+    for s in small:
+        p=np.where(temp==s)
+        d=np.sum(abs(data[p]-coords))
+        if d<diff:
+            diff=d
+            point=p
+    return point
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_clusters', default=50, type=int, help='number of clusters for kmeans')
+    parser.add_argument('--input_window', default=8, type=int, help='number of frames for the input data')
+    parser.add_argument('--output_window', default=0, type=int, help='number of frames for the output data')
+    parser.add_argument('--group_size', default=2, type=int, help='number of people to include in each dist group')
+    parser.add_argument('--dist_thresh', default=0.01, type=int, help='max avg distance to count people in the same social group')
+    parser.add_argument('--traj_thresh', default=2,  help='number of trajectories in each data point')
+    parser.add_argument('--social_thresh', default=2,  help='number of social groups in each data point')
+    args = parser.parse_args()
+    return args
+
+
+if __name__=='__main__':
+    args=get_args()
+    data=defaultdict(list)
+
+    # get the data from each dataset
+    for name in ['ETH', 'ETH_Hotel', 'UCY_Zara1', 'UCY_Zara2']:
+        dataset=SameSizeData(name, output_window=args.output_window, group_size=args.group_size, distThresh=args.dist_thresh,
+                             trajThresh=args.traj_thresh, socialThresh=args.social_thresh)
+        for d in dataset:
+            if len(d['pos'])>0:
+                data['pos'].extend([d['pos']])
+                data['peopleIDs'].extend([d['peopleIDs']])
+            if len(d['deltas'])>0:
+                data['deltas'].extend([d['deltas']])
+                data['groupIDs'].extend([d['groupIDs']])
+            if len(d['frames'])>0:
+                data['frames'].append(d['frames'])
+
+    print("flattening the data")
+    #flatten it and keep track of the index for the frames
+    if args.traj_thresh is not None:
+        positions=[]
+        #TODo: keep track of peopleIDs???
+        posFrames=[]
+        for i,x in enumerate(data['pos']):
+            posFrames.extend([data['frames'][i]] * x.shape[0])
+            positions.extend(x.reshape(-1,1))
+
+    if args.social_thresh is not None:
+        groups=[]
+        #ToDo: keep track of groupIDs???
+        groupFrames=[]
+        for i,x in enumerate(data['deltas']):
+            groupFrames.extend([data['frames']] * x.shape[0])
+            groups.extend(x.reshape(-1,1))
+
+    tsne = TSNE()
+    trajData = tsne.fit_transform(positions)
+    breakpoint()
+    socData = tsne.fit_transform(groups)
+    breakpoint()
+
+    if args.traj_thresh is not None:
+        print('creating the tsne embedding')
+        trajData = tsne.fit_transform(positions)
+        #ToDo: save trajData, especially since positions wont change from run to run
+
+        print('plotting the data')
+        kmeans = KMeans(n_clusters=args.num_clusters, random_state=0).fit(trajData)
+        centers = kmeans.cluster_centers_
+
+        plt.scatter(trajData[:, 0], trajData[:, 1], c=kmeans.labels_, alpha=0.5)
+        plt.scatter(centers[:, 0], centers[:, 1], c='r')
+        plt.title(str(args.traj_thresh)+" Traj Data, "+str(args.num_clusters)+" Clusters, Len "+str(args.input_window+args.output_window))
+        plt.show()
+
+        # for i in range(max(kmeans.labels_)+1):
+        #     clusters=[x for j,x in enumerate(trajData) if kmeans.labels_[j]==i]
+        #     for c in clusters:
+        #         x = [p for j, p in enumerate(c) if j % 2 == 1]
+        #         y = [p for j, p in enumerate(c) if j % 2 == 0]
+        #         plt.plot(x,y)
+        #         plt.scatter(x[0],y[0],c='r')
+        #     plt.title('Cluster '+str(i))
+        #     plt.show()
+
+        breakpoint()
+        for a in range(10):
+            coords = []
+            fig = plt.figure()
+            cid = fig.canvas.mpl_connect('button_press_event', onclick)
+            plt.scatter(trajData[:, 0], trajData[:, 1], c=kmeans.labels_, alpha=0.5)
+            plt.scatter(centers[:, 0], centers[:, 1], c='r')
+            plt.title(str(args.traj_thresh)+" Traj Data, "+str(args.num_clusters)+" Clusters, Len "+str(args.input_window+args.output_window))
+            plt.show()
+
+            point1 = find_nearest(tsne.embedding_[:, 0], coords[0][0])
+            point2 = find_nearest(tsne.embedding_[:, 1], coords[0][1])
+
+            diff1 = np.abs(data[point1] - coords)
+            diff2 = np.abs(data[point2] - coords)
+            if np.sum(diff1) <= np.sum(diff2):
+                point = point1[0][0]
+            else:
+                point = point2[0][0]
+
+            plt.scatter(trajData[:, 0], trajData[:, 1], c=kmeans.labels_, alpha=0.5)
+            plt.scatter(centers[:, 0], centers[:, 1], c='r')
+            plt.scatter(trajData[point][0], trajData[point][1], c='r', alpha=1)
+            plt.title(str(args.traj_thresh)+" Traj Data, "+str(args.num_clusters)+" Clusters, Len "+str(args.input_window+args.output_window))
+            plt.show()
+            plt.pause(.5)
+            plt.close()
+
+
+            for f in data['frames'][point]:
+                # import pdb; pdb.set_trace()
+                cv2.imshow('',f.numpy())
+                cv2.waitKey(1000)
+
+    if args.social_thresh is not None:
+        print('creating the tsne embedding')
+        socData = tsne.fit_transform(groups)
+        #ToDo: save socData
+
+        kmeans = KMeans(n_clusters=args.num_clusters, random_state=0).fit(socData)
+        centers = kmeans.cluster_centers_
+
+        print('plotting the data')
+        plt.scatter(socData[:, 0], socData[:, 1], c=kmeans.labels_, alpha=0.5)
+        plt.scatter(centers[:, 0], centers[:, 1], c='r')
+        plt.title(str(args.soc_thresh) + " Soc Dist Data, " + str(args.num_clusters) + " Clusters, Len " +
+                  str(args.input_window + args.output_window) + ", " + str(args.group_size) + "Group Size, "+
+                  str(args.dist_thresh)+" Dist thresh")
+        plt.show()
+
+        # for i in range(max(kmeans.labels_)+1):
+        #     clusters=[x for j,x in enumerate(trajData) if kmeans.labels_[j]==i]
+        #     for c in clusters:
+        #         x = [p for j, p in enumerate(c) if j % 2 == 1]
+        #         y = [p for j, p in enumerate(c) if j % 2 == 0]
+        #         plt.plot(x,y)
+        #         plt.scatter(x[0],y[0],c='r')
+        #     plt.title('Cluster '+str(i))
+        #     plt.show()
+
+        for a in range(10):
+            coords = []
+            fig = plt.figure()
+            cid = fig.canvas.mpl_connect('button_press_event', onclick)
+            plt.scatter(data[:, 0], data[:, 1], c=kmeans.labels_, alpha=0.5)
+            plt.scatter(centers[:, 0], centers[:, 1], c='r')
+            plt.title(str(args.data).title()+" Raw Data and Centroids, "+str(args.num_clusters)+" Clusters, Len "+str(args.seq_len))
+            plt.show()
+
+            point1 = find_nearest(tsneA.embedding_[:, 0], coords[0][0])
+            point2 = find_nearest(tsneA.embedding_[:, 1], coords[0][1])
+
+            diff1 = np.abs(data[point1] - coords)
+            diff2 = np.abs(data[point2] - coords)
+            if np.sum(diff1) <= np.sum(diff2):
+                point = point1[0][0]
+            else:
+                point = point2[0][0]
+
+            plt.scatter(data[:, 0], data[:, 1], c=kmeans.labels_, alpha=0.5)
+            plt.scatter(centers[:, 0], centers[:, 1], c='r')
+            plt.scatter(data[point][0], data[point][1], c='r', alpha=1)
+            plt.title(str(args.data).title() + " Raw Data and Centroids, " + str(args.num_clusters) + " Clusters, Len " + str(args.seq_len))
+            plt.show()
+            plt.pause(.5)
+            plt.close()
+
+            x=[p for i,p in enumerate(fullData[point]) if i%2==1]
+            y = [p for i, p in enumerate(fullData[point]) if i % 2 == 0]
+            plt.plot(x,y)
+            plt.title('Clicked Trajectory')
+            plt.show()
---
+++ sameSizeData.py
@@ -0,0 +1,234 @@
+from torch.utils.data import Dataset, DataLoader
+import cv2
+from torchvision.transforms import Compose, Resize, GaussianBlur
+import numpy as np
+from OpenTraj.opentraj.toolkit.loaders.loader_eth import load_eth
+from OpenTraj.opentraj.toolkit.loaders.loader_crowds import load_crowds
+from collections import defaultdict
+from OpenTraj.utils import world2image
+import torch
+from itertools import combinations
+
+class SameSizeData(Dataset):
+    def __init__(self, dataset, mode='by_frame', image=False, input_window=8, output_window=12, group_size=2,
+                 distThresh=0.01, trajThresh=None, socialThresh=None):
+        '''
+            dataset: selects which data paths to use from below
+            mode: either chooses the trajectories "by_frame" or "by_human"
+            image: False if need none; 'mask' if want masks; True if want raw images
+            input_window: number of time steps in the input data
+            output_window: number of time steps in the output data
+            group_size: number of people to consider as being a "group" when computing distances
+            distThresh: max avg distance a group's members can have while still being considered "close to each other"
+            trajTrhesh: max number of people returned in one data point when considering raw trajectories; None means don't filter
+            socialThresh: max number of social group distances returned in one data point; None means don't filter
+        '''
+        self.root = '/Users/faith_johnson/GitRepos/PedTrajPred/OpenTraj/'
+        self.name = dataset
+        # H path, Vid Path, load_X arguments
+        self.paths = {
+            'ETH': ['datasets/ETH/seq_eth/H.txt', 'datasets/ETH/seq_eth/video.avi', '/datasets/ETH/seq_eth/obsmat.txt', (13.8689, 13.8689), (-6.5107, -6.2595)], # (480, 640), (14, 16), (-10,-10),
+            'ETH_Hotel': ['datasets/ETH/seq_hotel/H.txt', 'datasets/ETH/seq_hotel/video.avi',
+                          '/datasets/ETH/seq_hotel/obsmat.txt', (4.2894, 4.2920), (-10.2537, -10.0058)], #(576, 720), (5, 3), (-6,-9),
+            'UCY_Zara1': ['datasets/UCY/zara01/H.txt', 'datasets/UCY/zara01/video.avi',
+                          'datasets/UCY/zara01/annotation.vsp', (17.0080, 16.7517), (-1.3615, -1.1406)],
+            'UCY_Zara2': ['datasets/UCY/zara02/H.txt', 'datasets/UCY/zara02/video.avi',
+                          'datasets/UCY/zara02/annotation.vsp', (17.0984, 16.9911), (-1.4058, -1.2719)]}
+
+        self.mode = mode
+        self.image = image
+        self.transforms = Compose([GaussianBlur(5)])
+        self.input_window = input_window
+        self.output_window = output_window
+        self.group_size=group_size
+        self.dist_thresh=distThresh
+        self.traj_thresh=trajThresh
+        self.social_thresh=socialThresh
+        self.max_group_size=22 # at seq len = 8 (so input=8, output=0); actually by dataset its [22, 15, 18, 17]
+
+        try:
+            paths = self.paths[dataset]
+            self.min=np.array(paths[4])
+            self.max=np.array(paths[3])
+        except Exception as e:
+            print(e)
+            print('Unsupported Dataset for OpTrajData:', dataset)
+            breakpoint()
+
+        self.H = np.loadtxt(self.root + paths[0])
+        self.video = cv2.VideoCapture(self.root + paths[1])
+
+        if 'ETH' in dataset:
+            self.H = np.linalg.inv(self.H)
+            self.dataset = load_eth(self.root + paths[2])
+        elif 'UCY' in dataset:
+            print('WARNING: MASKS/IMAGES ARE NOT CURRENTLY SUPPORTED FOR THIS DATASET:', dataset)
+            self.dataset = load_crowds(self.root + paths[2], use_kalman=False, homog_file=self.root + paths[0])
+
+        if self.mode == 'by_human':
+            self.trajectory = self.dataset.get_trajectories()
+            self.groups = self.trajectory.indices
+
+    def __len__(self):
+        if self.mode=='by_human':
+            return len(self.groups)
+        elif self.mode =='by_frame':
+            return self.dataset.data['frame_id'].nunique()-self.output_window-self.input_window
+
+    def __getitem__(self, item):
+        if self.mode == 'by_human':
+            data = self.getOneHumanTraj(item)
+        elif self.mode=='by_frame':
+            data = self.getFrames(item,self.output_window+self.input_window)
+            #turns the dict into an array if not filter
+            #or if filter, it gets rid of people not present in the entire sequence of frames
+            data = self.filterLength(data)
+            data = self.getDistances(data)
+            if self.social_thresh is not None:
+                # using social groups
+                data = self.trimSocialGroups(data)
+            if self.traj_thresh is not None:
+                # using raw trajectories
+                data = self.collateRemainders(data)
+            if len(data['pos'])==0 and len(data['deltas'])==0:
+                data['frames']=np.array([])
+
+        if self.image:
+            data['frames']=self.getImages(data)
+        data['index']=[item]
+        return data
+
+    def collateRemainders(self, data):
+        if len(data['pos'])> self.traj_thresh:
+            data['pos']=np.array(list(combinations(data['pos'],self.traj_thresh)))
+            data['peopleIDs']=np.array(list(combinations(data['peopleIDs'], self.traj_thresh)))
+        elif len(data['pos'])< self.traj_thresh:
+            data['pos']=np.array([])
+            data['peopleIDs']=np.array([])
+        return data
+
+    def trimSocialGroups(self, data):
+        if len(data['deltas'])<self.social_thresh:
+            data['deltas']=np.array([])
+            data['groupIDs']=np.array([])
+        elif len(data['deltas'])>self.social_thresh:
+            data['deltas'] = np.array(list(combinations(data['deltas'], self.social_thresh)))
+            data['groupIDs'] = np.array(list(combinations(data['groupIDs'], self.social_thresh)))
+        return data
+
+    def reset(self, data):
+        for k in data.keys():
+            data[k]=np.array([])
+        return data
+
+    def getImages(self,data):
+        # breakpoint()
+        inds = data['frames']
+        frames=[]
+        ret, f = self.video.read()
+        imsize=f.shape
+        for t,i in enumerate(inds):
+            if self.image=='mask':
+                f = np.zeros(imsize)
+                locs=data['pos'][:,t,:]
+                for l in locs:
+                    pix_loc = world2image(np.array([l]), self.H)
+                    cv2.circle(f, tuple(pix_loc[0]), 5, (255, 255, 255), -1)
+                f=self.transforms(torch.FloatTensor(f))
+            else:
+                self.video.set(1, i)  # 1 is for CV_CAP_PROP_POS_FRAMES
+                ret, f = self.video.read()
+            frames.append(f)
+        return frames
+
+    def getDistances(self, data):
+        ''' So Far, going with avg distance over the trajectory needs to be below a threshold '''
+        people = data['peopleIDs']
+        conversion={}
+        for i, p in enumerate(people):
+            conversion[p]=i
+        potential_groups=list(combinations(people, 2)) # get distance between every combo of 2 people
+        diffs = [data['pos'][conversion[x[0]]] - data['pos'][conversion[x[1]]] for x in potential_groups]
+        if self.group_size >2:
+            # went with average distance between each pair in the group
+            groups=list(combinations(people, self.group_size))
+            subgroups=[list(combinations(x,2)) for x in groups]
+            inds = [[potential_groups.index(x) for x in y] for y in subgroups]
+            diffs = np.array([np.sum(np.array(diffs)[x], axis=0) for x in inds])/3.0
+        else:
+            groups=potential_groups
+
+        thresh = [np.mean(np.sum(x ** 2, axis=-1)) for x in diffs]
+        diffs=np.array(diffs)[np.array(thresh)<=self.dist_thresh]
+        data['deltas']=diffs
+        data['groupIDs']=np.array(groups)[np.array(thresh)<=self.dist_thresh]
+        return data
+
+    def filterLength(self, data):
+        peopleIDs, posDict, frames = data['peopleIDs'], data['pos'], data['frames']
+        positions=[]
+        people=[]
+        for i,k in enumerate(posDict.keys()):
+            if len(posDict[k])==self.output_window+self.input_window:
+                positions.append(posDict[k])
+                people.append(k)
+        if len(positions)>0:
+            data['pos']=(np.array(positions)-self.min)/(self.max-self.min)
+            data['peopleIDs']=np.array(people)
+        else:
+            data=self.reset(data)
+        return data
+
+    def getOneHumanTraj(self,item):
+        dataset={}
+        group=list(self.groups.keys())[item]
+        data=self.trajectory.get_group(group)
+        dataset['frames']=data['frame_id'].tolist()
+        dataset['pos'] = data.filter(['pos_x', 'pos_y']).to_numpy()
+        return dataset
+
+    def getFrames(self, item, frameNumber):
+        #breakpoint()
+        peopleIDs, posDict, frames = [], defaultdict(list), [self.dataset]
+        for i in range(frameNumber):
+            frameID = [self.dataset.data['frame_id'].unique()[item+i]]
+            frames.extend(frameID)
+            people = self.dataset.get_frames(frameID)[0]
+            peopleIDs.append(people['agent_id'].to_numpy())
+            pos=people.filter(['pos_x', 'pos_y']).to_numpy()
+            for j,p in enumerate(peopleIDs[-1]):
+                posDict[p].append(pos[j])
+
+        dataset={'peopleIDs':peopleIDs, 'pos':posDict, 'frames':frames}
+        return dataset
+
+
+if __name__=='__main__':
+    x = SameSizeData(dataset='ETH_Hotel', output_window=0, group_size=3, socialThresh=3, trajThresh=3)
+    d=DataLoader(x,batch_size=1,shuffle=False)
+    # data=x.__getitem__(1) #78 gets none
+    # # import pdb; pdb.set_trace()
+    # for pid, pos, targ, img in d:
+    #     try:
+    #         # import pdb; pdb.set_trace()
+    #         cv2.imshow('',img[0].numpy()[0])
+    #         cv2.waitKey(100)
+    #         # print(pos)
+    #         # print(targ)
+    #     except:
+    #         import pdb; pdb.set_trace()
+    # breakpoint()
+    # fram=data['frames']
+    # for f in fram:
+    #     # import pdb; pdb.set_trace()
+    #     cv2.imshow('',f.numpy())
+    #     cv2.waitKey(1000)
+    for name in ['ETH', 'ETH_Hotel', 'UCY_Zara1', 'UCY_Zara2']:
+        x = SameSizeData(dataset=name, output_window=0, group_size=2, socialThresh=3, trajThresh=3)
+        d = DataLoader(x, batch_size=1, shuffle=False)
+        for p in d:
+            print(p['pos'].shape)
+            print(p['deltas'].shape)
+            breakpoint()
+
+    breakpoint()
